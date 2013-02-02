0e4095631acf5c0b4635d01c489369927a5d863a
arinrb <arinrb@gmail.com>
2013-02-01 21:35:51 -0800

    Added unique functions and subs
    Added unique functions and suboutines that are all under the interface
    unique.

---
+++ src/unique_func_dble.f90
@@ -0,0 +1,36 @@
+function unique_func_dble(x) result(out)
+  double precision, intent(in) :: x(:)
+  double precision, allocatable :: out(:)
+  double precision :: list(size(x))
+  integer :: i,j
+  logical :: inc(size(x))
+  inc = .true.
+  call DCOPY(size(x),x,1,list,1)
+  call sort(list)
+  i = 1
+  do
+     if (i > size(x)) exit
+     j = i
+     do 
+        j = j + 1
+        if (j > size(x)) exit
+        if (list(i) == list(j)) then
+           inc(j) = .false.
+        else
+           i = j
+        end if
+     end do
+  end do
+  allocate(out(count(inc)))
+  j = 0
+  do i = 1,size(x)
+     if (inc(i)) then
+        j = j + 1
+        out(j) = list(i)
+     end if
+  end do
+end function unique_func_dble
+
+
+
+  
