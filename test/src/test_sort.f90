0f882226831b4e3103223ed3917cca36d12c6b83
arinrb <arinrb@gmail.com>
2013-02-05 18:16:08 -0800

    Added testing of sorting of identical lists
    Added testing of sorting of identical lists.

--- test/src/test_sort.f90
+++ test/src/test_sort.f90
@@ -1,7 +1,7 @@
 program test_sort
   use fort_arrange
   implicit none
-  integer, parameter :: n = 1000000
+  integer, parameter :: n = 10000000
   integer, allocatable :: x_int(:)
   real, allocatable :: x_real(:)
   double precision, allocatable :: x_dble(:)
@@ -25,13 +25,16 @@ program test_sort
   call cpu_time(t0)
   call sort(x_int)
   call cpu_time(t1)
-  print '("Integer: ",F12.6, "Sec")',t1 - t0
+  print '(/,"------------")'
+  print '("Integer")'
+  print '(F12.6, " sec")',t1 - t0
   i = is_sorted(x_int)
   if (i == 0) then
      print '("Success")'
   else
      print '("Fail")'
   end if
+  print '("------------")'
   deallocate(x_int)
 
   !Real
@@ -40,13 +43,16 @@ program test_sort
   call cpu_time(t0)
   call sort(x_real)
   call cpu_time(t1)
-  print '("Real: ",F12.6, "Sec")',t1 - t0
+  print '(/,"------------")'
+  print '("Real")'
+  print '(F12.6, " sec")',t1 - t0
   i = is_sorted(x_real)
   if (i == 0) then
      print '("Success")'
   else
      print '("Fail")'
   end if
+  print '("------------")'
   deallocate(x_real)
 
   !Double Precision
@@ -55,12 +61,75 @@ program test_sort
   call cpu_time(t0)
   call sort(x_dble)
   call cpu_time(t1)
-  print '("Double Precision: ",F12.6, "Sec")',t1 - t0
+  print '(/,"------------")'
+  print '("Double Precision")'
+  print '(F12.6, " sec")',t1 - t0
   i = is_sorted(x_dble)
   if (i == 0) then
      print '("Success")'
   else
      print '("Fail")'
   end if
+  print '("------------")'
   deallocate(x_dble)
+
+  !Identical Lists
+  print '(/,"============")'
+  print '("Identical Lists")'
+  print '("============")'
+
+  !Integer
+  allocate(x_int(n))
+  x_int = 1
+  call cpu_time(t0)
+  call sort(x_int)
+  call cpu_time(t1)
+  print '(/,"------------")'
+  print '("Integer")'
+  print '(F12.6, " sec")',t1 - t0
+  i = is_sorted(x_int)
+  if (i == 0) then
+     print '("Success")'
+  else
+     print '("Fail")'
+  end if
+  print '("------------")'
+  deallocate(x_int)
+
+  !Real
+  allocate(x_real(n))
+  x_real = 1.0
+  call cpu_time(t0)
+  call sort(x_real)
+  call cpu_time(t1)
+  print '(/,"------------")'
+  print '("Real")'
+  print '(F12.6, " sec")',t1 - t0
+  i = is_sorted(x_real)
+  if (i == 0) then
+     print '("Success")'
+  else
+     print '("Fail")'
+  end if
+  print '("------------")'
+  deallocate(x_real)
+
+  !Double Precision
+  allocate(x_dble(n))
+  x_dble = 1.0D0
+  call cpu_time(t0)
+  call sort(x_dble)
+  call cpu_time(t1)
+  print '(/,"------------")'
+  print '("Double Precision")'
+  print '(F12.6, " sec")',t1 - t0
+  i = is_sorted(x_dble)
+  if (i == 0) then
+     print '("Success")'
+  else
+     print '("Fail")'
+  end if
+  print '("------------")'
+  deallocate(x_dble)
+
 end program test_sort
