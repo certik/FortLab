666004cec4a67542b5bcf7385d315ad8dae3274a
arinrb <arinrb@gmail.com>
2013-02-06 07:44:13 -0800

    Added test of identical map
    Added a test for sort map of identical lists.

--- test/src/test_sort.f90
+++ test/src/test_sort.f90
@@ -2,19 +2,16 @@ program test_sort
   use fort_arrange
   implicit none
   integer, parameter :: n = 10000000
-  integer, allocatable :: x_int(:)
-  real, allocatable :: x_real(:)
-  double precision, allocatable :: x_dble(:)
+  integer, allocatable :: x_int(:),y_int(:),map(:)
+  real, allocatable :: x_real(:),y_real(:)
+  double precision, allocatable :: x_dble(:),y_dble(:)
   real :: t0,t1,x
   integer :: i
+  character(len=80) :: time,passP
   
-  print '("============")'
-  print '("Number of Elements ",/,12I,/)',n
-  print '("============")'
-  !Random List
-  print '(/,"============")'
-  print '("Random Lists")'
-  print '("============")'
+  print '(/,"============================================================")'
+  print '("    Test Sort of Random Lists")'
+  print '("============================================================")'
 
   !Integer
   allocate(x_int(n))
@@ -25,16 +22,14 @@ program test_sort
   call cpu_time(t0)
   call sort(x_int)
   call cpu_time(t1)
-  print '(/,"------------")'
-  print '("Integer")'
-  print '(F12.6, " sec")',t1 - t0
   i = is_sorted(x_int)
   if (i == 0) then
-     print '("Success")'
+     write(passP,'("Pass")')
   else
-     print '("Fail")'
+     write(passP,'("Fail")')
   end if
-  print '("------------")'
+  write(time,*) t1-t0
+  print '("Integer          ",A,"sec ",A)', trim(time), trim(passP)
   deallocate(x_int)
 
   !Real
@@ -43,16 +38,14 @@ program test_sort
   call cpu_time(t0)
   call sort(x_real)
   call cpu_time(t1)
-  print '(/,"------------")'
-  print '("Real")'
-  print '(F12.6, " sec")',t1 - t0
   i = is_sorted(x_real)
   if (i == 0) then
-     print '("Success")'
+     write(passP,'("Pass")')
   else
-     print '("Fail")'
+     write(passP,'("Fail")')
   end if
-  print '("------------")'
+  write(time,*) t1-t0
+  print '("Real             ",A,"sec ",A)', trim(time), trim(passP)
   deallocate(x_real)
 
   !Double Precision
@@ -61,22 +54,20 @@ program test_sort
   call cpu_time(t0)
   call sort(x_dble)
   call cpu_time(t1)
-  print '(/,"------------")'
-  print '("Double Precision")'
-  print '(F12.6, " sec")',t1 - t0
   i = is_sorted(x_dble)
   if (i == 0) then
-     print '("Success")'
+     write(passP,'("Pass")')
   else
-     print '("Fail")'
+     write(passP,'("Fail")')
   end if
-  print '("------------")'
+  write(time,*) t1-t0
+  print '("Double Precision ",A,"sec ",A)', trim(time), trim(passP)
   deallocate(x_dble)
 
   !Identical Lists
-  print '(/,"============")'
-  print '("Identical Lists")'
-  print '("============")'
+  print '(/,"============================================================")'
+  print '("    Test Sort of Identical Lits")'
+  print '("============================================================")'
 
   !Integer
   allocate(x_int(n))
@@ -84,16 +75,14 @@ program test_sort
   call cpu_time(t0)
   call sort(x_int)
   call cpu_time(t1)
-  print '(/,"------------")'
-  print '("Integer")'
-  print '(F12.6, " sec")',t1 - t0
   i = is_sorted(x_int)
   if (i == 0) then
-     print '("Success")'
+     write(passP,'("Pass")')
   else
-     print '("Fail")'
+     write(passP,'("Fail")')
   end if
-  print '("------------")'
+  write(time,*) t1-t0
+  print '("Integer          ",A,"sec ",A)', trim(time), trim(passP)
   deallocate(x_int)
 
   !Real
@@ -102,16 +91,14 @@ program test_sort
   call cpu_time(t0)
   call sort(x_real)
   call cpu_time(t1)
-  print '(/,"------------")'
-  print '("Real")'
-  print '(F12.6, " sec")',t1 - t0
   i = is_sorted(x_real)
   if (i == 0) then
-     print '("Success")'
+     write(passP,'("Pass")')
   else
-     print '("Fail")'
+     write(passP,'("Fail")')
   end if
-  print '("------------")'
+  write(time,*) t1-t0
+  print '("Real             ",A,"sec ",A)', trim(time), trim(passP)
   deallocate(x_real)
 
   !Double Precision
@@ -120,16 +107,136 @@ program test_sort
   call cpu_time(t0)
   call sort(x_dble)
   call cpu_time(t1)
-  print '(/,"------------")'
-  print '("Double Precision")'
-  print '(F12.6, " sec")',t1 - t0
   i = is_sorted(x_dble)
   if (i == 0) then
-     print '("Success")'
+     write(passP,'("Pass")')
   else
-     print '("Fail")'
+     write(passP,'("Fail")')
   end if
-  print '("------------")'
+  write(time,*) t1-t0
+  print '("Double Precision ",A,"sec ",A)', trim(time), trim(passP)
   deallocate(x_dble)
+
+  !Test Sort Map on Random List
+  print '(/,"============================================================")'
+  print '("    Test Sort Map of Random Lists")'
+  print '("============================================================")'
+
+  !Integer
+  allocate(x_int(n),y_int(n),map(n))
+  do i = 1,n
+     call random_number(x)
+     x_int(i) = floor(x * 10000)
+  end do
+  y_int = x_int
+  call cpu_time(t0)
+  call sort(y_int,map)
+  call cpu_time(t1)
+  x_int = x_int(map)
+  i = is_sorted(x_int)
+  if (i == 0) then
+     write(passP,'("Pass")')
+  else
+     write(passP,'("Fail")')
+  end if
+  write(time,*) t1-t0
+  print '("Integer          ",A,"sec ",A)', trim(time), trim(passP)
+  deallocate(x_int,y_int,map)
+
+  !Real
+  allocate(x_real(n),y_real(n),map(n))
+  call random_number(x_real)
+  y_real = x_real
+  call cpu_time(t0)
+  call sort(y_real,map)
+  call cpu_time(t1)
+  x_real = x_real(map)
+  i = is_sorted(x_real)
+  if (i == 0) then
+     write(passP,'("Pass")')
+  else
+     write(passP,'("Fail")')
+  end if
+  write(time,*) t1-t0
+  print '("Real             ",A,"sec ",A)', trim(time), trim(passP)
+  deallocate(x_real,y_real,map)
+
+  !Double Precision
+  allocate(x_dble(n),y_dble(n),map(n))
+  call random_number(x_dble)
+  y_dble = x_dble
+  call cpu_time(t0)
+  call sort(y_dble,map)
+  call cpu_time(t1)
+  x_dble = x_dble(map)
+  i = is_sorted(x_dble)
+  if (i == 0) then
+     write(passP,'("Pass")')
+  else
+     write(passP,'("Fail")')
+  end if
+  write(time,*) t1-t0
+  print '("Double Precision ",A,"sec ",A)', trim(time), trim(passP)
+  deallocate(x_dble,y_dble,map)
+
+
+  !Test Sort Map on Random List
+  print '(/,"============================================================")'
+  print '("    Test Sort Map of Indentical Lists")'
+  print '("============================================================")'
+
+  !Integer
+  allocate(x_int(n),y_int(n),map(n))
+  x_int = 1
+  y_int = x_int
+  call cpu_time(t0)
+  call sort(y_int,map)
+  call cpu_time(t1)
+  x_int = x_int(map)
+  i = is_sorted(x_int)
+  if (i == 0) then
+     write(passP,'("Pass")')
+  else
+     write(passP,'("Fail")')
+  end if
+  write(time,*) t1-t0
+  print '("Integer          ",A,"sec ",A)', trim(time), trim(passP)
+  deallocate(x_int,y_int,map)
+
+  !Real
+  allocate(x_real(n),y_real(n),map(n))
+  x_real = 1.0
+  y_real = x_real
+  call cpu_time(t0)
+  call sort(y_real,map)
+  call cpu_time(t1)
+  x_real = x_real(map)
+  i = is_sorted(x_real)
+  if (i == 0) then
+     write(passP,'("Pass")')
+  else
+     write(passP,'("Fail")')
+  end if
+  write(time,*) t1-t0
+  print '("Real             ",A,"sec ",A)', trim(time), trim(passP)
+  deallocate(x_real,y_real,map)
+
+  !Double Precision
+  allocate(x_dble(n),y_dble(n),map(n))
+  x_dble = 1.0D0
+  y_dble = x_dble
+  call cpu_time(t0)
+  call sort(y_dble,map)
+  call cpu_time(t1)
+  x_dble = x_dble(map)
+  i = is_sorted(x_dble)
+  if (i == 0) then
+     write(passP,'("Pass")')
+  else
+     write(passP,'("Fail")')
+  end if
+  write(time,*) t1-t0
+  print '("Double Precision ",A,"sec ",A)', trim(time), trim(passP)
+  deallocate(x_dble,y_dble,map)
 
 end program test_sort
