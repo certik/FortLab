53a1f5de08b666035a1b9efd80e39a156586e49e
arinrb <arinrb@gmail.com>
2013-02-17 20:13:22 -0800

    Rewrite max_interchange_sort_size

--- fort_arrange/src/set_max_interchange_sort_size.f90
+++ fort_arrange/src/set_max_interchange_sort_size.f90
@@ -1,16 +1,5 @@
 subroutine set_max_interchange_sort_size(x)
   integer, intent(in) :: x
-  character(len=16) :: x_char
-  if (x < 4) then
-     write(x_char,*) x
-     write(error_unit,10) trim(x_char)
-     stop
-  else
-     max_interchange_sort_size = x
-  end if
-10 format("Error: max interchange sort size cannot be set" //&
-        "to values lower than 4 to prevent quick sort from "//&
-        "breaking.  A value of ",A," was passed.")
-     
+  max_interchange_sort_size = max(2,x)
 end subroutine set_max_interchange_sort_size
 
